c081dfa7b5b9f5f4621e0012e5eed69240f8204f
Ignacio Padilla <chavez.ignacio@uabc.edu.mx>
2019-12-23 16:19:02 -0800

    challenge 2 complete

--- src/solutions/_0002.java
+++ src/solutions/_0002.java
@@ -1,7 +1,5 @@
 package solutions;
 
-
-
 /**
  * >>add two number>>
  * 
@@ -43,27 +41,77 @@ public class _0002 {
 			}
 			return node;
 		}
+
+		void display() {
+			ListNode list = this;
+
+			do {
+				System.out.print(list.val + ((list.next == null) ? "\n" : " -> "));
+				list = list.next;
+			} while (list != null);
+		}
 	}
 
 	static class Solution {
-
+		//my own solution.
 		static ListNode bruteForce(ListNode l1, ListNode l2) {
-			return l2;
+
+			ListNode result = null; // List's head
+			ListNode aux = null;
+			boolean carry = false;
+
+			do {
+				int n0 = (l1 == null) ? 0 : l1.val;
+				int n1 = (l2 == null) ? 0 : l2.val;
+				int sum = n0 + n1 + ((carry) ? 1 : 0);
+				carry = (sum > 9) ? true : false;
+
+				if (result == null)
+					aux = result = new ListNode(sum % 10);
+				else
+					aux = aux.next = new ListNode(sum % 10); // sum%10 wraps the number in case sum exceeds 9.
+
+				l1 = (l1 != null) ? l1.next : l1; // gettin' next node
+				l2 = (l2 != null) ? l2.next : l2;
+
+			} while (l1 != null || l2 != null || carry);
+
+			return result;
 		}
 
+		//Given answer.
 		static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-			return l2;
+
+			ListNode dummyHead = new ListNode(0);
+			ListNode p = l1, q = l2, curr = dummyHead;
+			int carry = 0;
+			while (p != null || q != null) {
+				int x = (p != null) ? p.val : 0;
+				int y = (q != null) ? q.val : 0;
+				int sum = carry + x + y;
+				carry = sum / 10;
+				curr.next = new ListNode(sum % 10);
+				curr = curr.next;
+				if (p != null)
+					p = p.next;
+				if (q != null)
+					q = q.next;
+			}
+			if (carry > 0) {
+				curr.next = new ListNode(carry);
+			}
+			return dummyHead.next;
 		}
 	}
 
 	public static void main(String[] args) {
 		ListNode n1 = new ListNode(2);
 		ListNode n2 = new ListNode(5);
-		
+
 		n1.add(new ListNode(4)).add(new ListNode(3));
 		n2.add(new ListNode(6)).add(new ListNode(4));
-		
 
-		
+		Solution.bruteForce(n1, n2).display();
 	}
 }
+//23-dec-2019 16:16
